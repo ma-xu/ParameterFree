f50d02535c4b747e2e92281e4338802f826bbec6
13952522076 <xuma@my.unt.edu>
2020-01-22 23:22:01 -0600

    init

---
+++ models/resnet/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+
+############## ResNet #########################
+# Original
+# from .resnet_old import *
+# from .resnet_se import *
+# from .resnet_sge import *
+
+
+from .resnet_pf import *
+from .resnet_pf2 import *
