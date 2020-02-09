eb1774434ce39746f99140082eef417f21fcca74
13952522076 <xuma@my.unt.edu>
2020-02-08 18:02:21 -0600

    111

--- models/resnet/__init__.py
+++ models/resnet/__init__.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import
 # from .resnet_se import *
 # from .resnet_sge import *
 
-
+from .resnet_old import *
 from .resnet_pf import *
 from .resnet_pf2 import *
 from .resnet_pf4 import *
@@ -17,6 +17,7 @@ from .resnet_pf7 import *
 from .resnet_pf8 import *
 from .resnet_pf9 import *
 from .resnet_pf10 import *
+from .resnet_pf10_2 import *
 from .resnet_pf11 import *
 from .resnet_pf12 import *
 from .resnet_pf13 import *
